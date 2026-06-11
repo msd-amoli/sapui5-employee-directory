796342f242dbfba6c9e70ea489a5b653715f6ab1
Madhusudan Amoli <2520020051@geu.ac.in>
2026-06-11 08:41:38 +0000

    Employee data with filter and selection change on table

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("student.com.sap.training.advancedsapui5.employeedirectory.controller.Main", {
@@ -16,12 +18,40 @@ sap.ui.define([
         },
         onEmployeeSelect:function(evt){
             console.log("event fired");
-            const itm = evt.getParameter("listItem").getBindingContext("emp");
-            const empobj = itm.getObject();
+            const itm = evt.getParameter("listItem").getBindingContext("emp");//get the selected item and its binding context
+            const empobj = itm.getObject(); //get the object of the selected item
            
             const md = this.getView().getModel("emp");
             md.setProperty("/selectedEmployee",empobj);
             
+        },
+        onSearch:function(evt){
+            const sQuery = evt.getParameter("newValue"); //get the search query
+            const lst = this.byId("emplist");
+            const binding = lst.getBinding("items");
+            if(!sQuery){
+                binding.filter([]); //if search query is empty, remove all filters
+                return;
+            }
+            const oFilter = new Filter("name", FilterOperator.Contains, sQuery); //create a filter for the name property
+                // const oFilter = new sap.ui.model.Filter
+                // ("name",
+                //     sap.ui.model.FilterOperator.Contains,sQuery); //create a filter for the name property
+
+                
+            binding.filter([oFilter]);
+            console.log(oFilter);
+        },
+        onSearch2:function(evt){
+            const qry = evt.getParameter("newValue");
+            const lst = this.byId("emplist");
+            const binding = lst.getBinding("items");
+            if(!qry){
+                binding.filter([]);
+                return;
+            }
+            const ofilter = new Filter("department", FilterOperator.Contains,qry);
+            binding.filter([ofilter]);
         }
     });
 });
